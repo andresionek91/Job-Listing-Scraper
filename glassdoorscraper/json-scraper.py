c9c3fe19c6b6eb05aced3fa202ce97e3f5e9de3a
Andre Sionek <andresionek91@gmail.com>
2019-11-10 12:02:07 +0000

    create function to download data from specific job ids

---
+++ glassdoorscraper/json-scraper.py
@@ -0,0 +1,65 @@
+import requests
+import yaml
+import logging
+from multiprocessing import Pool, Manager
+import json
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+with open('config.yml') as config_file:
+    config = yaml.safe_load(config_file)
+
+
+def load_unique_job_ids():
+    job_ids = set()
+    with open(config['jobs-ids-filename']) as jobs_ids_file:
+        for row in jobs_ids_file:
+            job_ids.add(row)
+    return list(job_ids)
+
+
+def get_job_info(job_id):
+    url = 'https://www.glassdoor.co.uk/Job/json/details.htm?jobListingId={job_id}'.format(job_id=job_id)
+    json_data = requests.get(url, headers=config['headers']).json()
+    return json_data
+
+
+def write_json(data, idx):
+    with open('results/file_{}.json'.format(idx), 'w') as json_file:
+        for result in data:
+            json_file.write(json.dumps(result, ensure_ascii=False))
+            json_file.write('\n')
+    logging.info('file {} written'.format(idx))
+
+
+def worker(row, job_id):
+    json_data = get_job_info(job_id)
+    row.append(json_data)
+
+
+def divide_chunks(data, size):
+    for i in range(0, len(data), size):
+        yield data[i:i + size]
+
+
+def execute():
+    data = load_unique_job_ids()
+    chunks = divide_chunks(data, 200)
+
+    for idx, chunk in enumerate(chunks):
+        with Manager() as mgr:
+            row = mgr.list([])
+
+            params = []
+            for job_id in chunk:
+                params.append([row, job_id])
+
+            with Pool() as p:
+                p.starmap(worker, params)
+
+            write_json(row, idx)
+
+
+if __name__ == '__main__':
+    execute()
